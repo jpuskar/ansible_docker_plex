ecd29f561310a7095ef1a12abfcb8138174bfca1
John Puskar <johnpuskar@gmail.com>
2026-01-10 12:05:27 -0500

    testing adding passwd

--- ansible/roles/argocd-apps-nvidia-device-plugin/filter_plugins/patch_nfd_worker.py
+++ ansible/roles/argocd-apps-nvidia-device-plugin/filter_plugins/patch_nfd_worker.py
@@ -25,8 +25,9 @@ def patch_nfd_worker_nodeSelector(helm_output, node_selector):
     # Parse the YAML documents (should be normalized by yq already)
     documents = list(yaml.safe_load_all(helm_output))
 
-    # Find and patch the NFD worker daemonset
+    # Find and patch the NFD worker daemonset and MPS control daemon
     for doc in documents:
+        # Patch NFD worker with nodeSelector
         if (doc and
             doc.get('kind') == 'DaemonSet' and
             doc.get('metadata', {}).get('name', '').endswith('node-feature-discovery-worker')):
@@ -37,6 +38,44 @@ def patch_nfd_worker_nodeSelector(helm_output, node_selector):
                 if 'nodeSelector' not in template_spec:
                     template_spec['nodeSelector'] = {}
                 template_spec['nodeSelector'].update(node_selector_plain)
+
+        # Patch MPS control daemon with passwd/group mounts for Talos
+        if (doc and
+            doc.get('kind') == 'DaemonSet' and
+            doc.get('metadata', {}).get('name', '').endswith('mps-control-daemon')):
+
+            if 'spec' in doc and 'template' in doc['spec'] and 'spec' in doc['spec']['template']:
+                template_spec = doc['spec']['template']['spec']
+
+                # Add ConfigMap volume
+                if 'volumes' not in template_spec:
+                    template_spec['volumes'] = []
+                template_spec['volumes'].append({
+                    'name': 'etc-passwd',
+                    'configMap': {
+                        'name': 'minimal-passwd'
+                    }
+                })
+
+                # Add volume mounts to all containers
+                if 'containers' in template_spec:
+                    for container in template_spec['containers']:
+                        if 'volumeMounts' not in container:
+                            container['volumeMounts'] = []
+                        container['volumeMounts'].extend([
+                            {
+                                'name': 'etc-passwd',
+                                'mountPath': '/etc/passwd',
+                                'subPath': 'passwd',
+                                'readOnly': True
+                            },
+                            {
+                                'name': 'etc-passwd',
+                                'mountPath': '/etc/group',
+                                'subPath': 'group',
+                                'readOnly': True
+                            }
+                        ])
 
     # Convert back to YAML string
     output_parts = []
