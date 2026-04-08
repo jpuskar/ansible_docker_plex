6fbbaaec0e9688dd09b3b9d6b500f2fa8d320c75
John Puskar <john.puskar@amtrustgroup.com>
2026-04-08 17:11:15 -0400

    fix rcon backup

--- docker-images/palworld-rcon-backup/start.sh
+++ docker-images/palworld-rcon-backup/start.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
+if [[ -z "${MY_NAMESPACE:-}" ]]; then
+  echo "ERROR: MY_NAMESPACE env var is not set. Exiting."
+  exit 1
+fi
+
 echo "Waiting 30s for server to start."
 sleep 30
 
 while true; do
     set +e
-    python3 /run-backup.py --secret-namespace palworld0
+    python3 /run-backup.py --secret-namespace "$MY_NAMESPACE"
     if [ $? -eq 0 ]; then
         echo "Command executed successfully. Waiting for 5mins before saving again."
         # Sleep for 5 minutes before running the command again
