415a4b78055666d9eae6b607c84ade739a881b4b
John Puskar <john.puskar@amtrustgroup.com>
2026-05-16 10:43:25 -0400

    fix namespace labels

--- ansible/filter_plugins/patch_namespace_labels.py
+++ ansible/filter_plugins/patch_namespace_labels.py
@@ -11,13 +11,22 @@ Where extra_labels is a dict, e.g.:
     "pod-security.kubernetes.io/warn":    "privileged" }
 """
 
+import json
+
 import yaml
+
+
+def _to_plain(obj):
+    """Convert Ansible variable types to plain Python types via JSON round-trip."""
+    return json.loads(json.dumps(obj))
 
 
 def patch_namespace_labels(raw_yaml, extra_labels):
     """Find every Namespace doc in a multi-doc YAML stream and merge labels."""
     if not extra_labels:
         return raw_yaml
+
+    plain_labels = _to_plain(extra_labels)
 
     docs_out = []
     for raw_doc in raw_yaml.split("\n---"):
@@ -39,7 +48,7 @@ def patch_namespace_labels(raw_yaml, extra_labels):
         # Merge labels into the Namespace document
         meta = doc.setdefault("metadata", {})
         labels = meta.setdefault("labels", {})
-        labels.update(extra_labels)
+        labels.update(plain_labels)
 
         # Re-serialize just this document
         dumped = yaml.dump(doc, default_flow_style=False, sort_keys=False)
